fa33ad159cb5d0221bc8143973a6b4034973a0ed
AmanyHerez <AmanyHerez2000@gmail.com>
2021-10-28 16:04:54 +0300

    first commit

---
+++ app/src/main/java/com/example/bmi_application/login_screen.java
@@ -0,0 +1,13 @@
+package com.example.bmi_application;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class login_screen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
